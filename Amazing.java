0318f69d984029b09ce2f6239fa71f2ba04f65b5
Angelica <Angelica137@users.noreply.github.com>
2021-03-14 12:30:34 +0000

    decalring the variables

--- Amazing.java
+++ Amazing.java
@@ -1,20 +1,19 @@
 
 /**
- * A class to hold the declarations and Expressions 
+ * A class to hold the declarations and Expressions
  * 
- * @author (your name) 
+ * @author (your name)
  * @version (a version number or a date)
  */
-public class Amazing
-{
-   //Put your data type declarations below. Here is an example
-    int n = 4;
-     
+public class Amazing {
+	// Put your data type declarations below. Here is an example
+	int n = 4;
+	float x = (float) 2.5;
+	float y = (float) -1.5;
+	int m = 18;
 
-//Put the code for your calculations in this method.   
-   void doCalculations()
-   {
-       
-       
-   }//End of doCalculations
+	// Put the code for your calculations in this method.
+	void doCalculations() {
+
+	}// End of doCalculations
 }
